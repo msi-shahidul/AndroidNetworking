ae0b8c0686dbcb8186153b4100e71e21eee72c9c
Shahidul Islam <SIslam@luxoft.com>
2020-10-23 21:49:38 +0200

    Added Loading Indicator

--- app/src/main/java/android/example/com/earthquack/MainActivity.java
+++ app/src/main/java/android/example/com/earthquack/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -21,6 +22,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -40,6 +42,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
     private EarthQuackAdapter mArrayAdapter;
 
+    /** TextView that is displayed when the list is empty */
+    private TextView mEmptyStateTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,11 +58,18 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         if (networkInfo != null && networkInfo.isConnected()) {
             LoaderManager.getInstance(this).initLoader(0, null, this);
         } else {
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
 
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
         }
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        earthquakeListView.setEmptyView(mEmptyStateTextView);
+
         mArrayAdapter = new EarthQuackAdapter(MainActivity.this,  new ArrayList<EarthQuackData>());
         earthquakeListView.setAdapter(mArrayAdapter);
 
@@ -84,7 +96,14 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(@NonNull Loader<ArrayList<EarthQuackData>> loader, ArrayList<EarthQuackData> data) {
-        mArrayAdapter.addAll(data);
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+        mEmptyStateTextView.setText(R.string.no_earthquakes);
+        if (data != null && !data.isEmpty()) {
+            // Set empty state text to display "No earthquakes found."
+            mEmptyStateTextView.setVisibility(View.GONE);
+            mArrayAdapter.addAll(data);
+        }
     }
 
     @Override
